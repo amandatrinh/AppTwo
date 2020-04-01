4323fe47ef941c53ec5c7a5b9b2b42fdbf001966
Amanda Trinh <amandt3@uci.edu>
2020-03-31 21:43:09 -0700

    Initial Commit

---
+++ AppTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppTwo
+//
+//  Created by Amanda on 1/11/20.
+//  Copyright © 2020 Amanda Trinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
